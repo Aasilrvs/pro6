edae946e1cd984adf723d171d902bcc19cb03b5a
Aasil Ahamed M <aasilahamed42@gmail.com>
2026-09-11 11:47:11 +0530

    Update student information and complete firewall commands

--- firewall_check.sh
+++ firewall_check.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
 # Linux Firewall Practical
-# Student Name:
-# Register Number:
+# Student Name:aasil ahamed
+# Register Number:1u24it001
 
 # 1. Check firewalld service status
-
+sudo systemctl status firewalld
 
 # 2. Start firewalld
-# TODO: Complete the command
-
+sudo systemctl start firewalld
 
 # 3. Enable firewalld at boot
-# TODO: Complete the command
-
+sudo systemctl enable firewalld
 
 # 4. Check firewall state
-# TODO: Complete the command
-
+sudo firewall-cmd --state
 
 # 5. Get default firewall zone
-# TODO: Complete the command
-
+sudo firewall-cmd --get-default-zone
 
 # 6. Get active firewall zones
-# TODO: Complete the command
-
+sudo firewall-cmd --get-active-zones
